e7392fb1cb0e6a311c43bc6f08aa8f9ac1019bf5
Rémy SUN <Rmy SUN>
2016-03-21 23:01:08 +0100

    added (empty...) save button

--- src/Gui.java
+++ src/Gui.java
@@ -1,23 +1,29 @@
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Gui {
 	private JFrame gui;
 	private StatDock statDock;
+	private Savestate savestate;
 	private int width = 400;
 	private int height = 600;
 	
 	public Gui() {
 		gui = new JFrame();
 		
+		gui.getContentPane().setLayout(new BorderLayout());
+		
 		gui.setSize(width, height);
 		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		statDock = new StatDock();
-
 		statDock.strPanel.setStat(5);
+		gui.add(statDock.main,BorderLayout.CENTER);
 		
-		gui.add(statDock.main);
+		savestate = new Savestate();
+		gui.add(savestate.saveButton,BorderLayout.SOUTH);
 		
 		gui.setVisible(true);
 	}
---
+++ src/Savestate.java
@@ -0,0 +1,9 @@
+import javax.swing.JButton;
+
+public class Savestate {
+	public JButton saveButton;
+	
+	public Savestate() {
+		saveButton = new JButton("Save!");
+	}
+}
